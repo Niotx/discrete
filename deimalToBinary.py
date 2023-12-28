927a0aae4dba4aad14252abe08a8b285d05a8e6e
Niotx <mmnaseri4444@gmail.com>
2023-12-28 17:55:06 +0330

    1.4 add def

--- deimalToBinary.py
+++ deimalToBinary.py
@@ -15,9 +15,8 @@ def binary(first):
         first = first / 2
         first = int(first)
         new = new - 1
-    #print(arr0[::-1])
-    print('*', arr0)
-    #print(len(arr0))
+    print('IRL', arr0[::-1])
+    print('P', arr0)
     return arr0
 
 
@@ -26,22 +25,92 @@ def cut(arr):
     flag = 0
     if num != 0:
         for i in range(num-1, -1, -1):
-            #print('*', i)
             if arr[i] == 1:
                 flag = i
                 break
             else:
                 del arr[i]
-        # print(flag, arr)
     else:
         print(arr)
-# def compare(arr1, arr2):
-#     if len(arr1) > len(arr2):
+    return arr
+
+
+def add(arr1, arr2):
+    num = 0
+    carry = 0
+    addarr = []
+    newadd = []
+    if len(arr1) > len(arr2):
+        num = len(arr1) - len(arr2)
+        for i in range(num):
+            arr2.append(0)
+        for j in range(len(arr2)):
+            if (arr1[j] + arr2[j] + carry) == 1:
+                carry = 0
+                addarr.append(1)
+                newadd.append(2 ** j)
+            elif (arr1[j] + arr2[j] + carry) == 2:
+                carry = 1
+                addarr.append(0)
+                newadd.append(0)
+                if len(arr2) == j + 1:
+                    addarr.append(1)
+                    newadd.append(2 ** (j + 1))
+            elif (arr1[j] + arr2[j] + carry) == 0:
+                carry = 0
+                addarr.append(0)
+                newadd.append(0)
+            elif (arr1[j] + arr2[j] + carry) == 3:
+                carry = 1
+                addarr.append(1)
+                newadd.append(2 ** j)
+                if len(arr2) == j+1:
+                    addarr.append(1)
+                    newadd.append(2 ** (j + 1))
+        print('aad: ', addarr, newadd)
+        print('aad IRL: ', addarr[::-1], newadd[::-1])
+    elif len(arr1) < len(arr2):
+        num = len(arr2) - len(arr1)
+        for i in range(num):
+            arr1.append(0)
+            print(arr1)
+        for j in range(len(arr2)):
+            if (arr1[j] + arr2[j] + carry) == 1:
+                carry = 0
+                addarr.append(1)
+                newadd.append(2 ** j)
+            elif (arr1[j] + arr2[j] + carry) == 2:
+                carry = 1
+                addarr.append(0)
+                newadd.append(0)
+                if len(arr2) == j + 1:
+                    addarr.append(1)
+                    newadd.append(2 ** (j + 1))
+            elif (arr1[j] + arr2[j] + carry) == 0:
+                carry = 0
+                addarr.append(0)
+                newadd.append(0)
+            elif (arr1[j] + arr2[j] + carry) == 3:
+                carry = 1
+                addarr.append(1)
+                newadd.append(2 ** j)
+                if len(arr2) == j+1:
+                    addarr.append(1)
+                    newadd.append(2 ** (j + 1))
+        print('aad: ', addarr, newadd)
+        print('aad IRL: ', addarr[::-1], newadd[::-1])
+    else:
+        print('same')
+        pass
+    return addarr
 
 
 firstInp = int(input("Enter first decimal number : "))
 secondInp = int(input("Enter second decimal number : "))
-binary(firstInp)
-binary(secondInp)
-cut(binary(firstInp))
+firstInp = binary(firstInp)
+secondInp = binary(secondInp)
+firstInp = cut(firstInp)
+secondInp = cut(secondInp)
+comp = add(firstInp,secondInp)
+print(firstInp, secondInp)
 
